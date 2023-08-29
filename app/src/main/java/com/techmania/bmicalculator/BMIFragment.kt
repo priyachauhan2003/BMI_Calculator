cc5df5c989dddd5af99334b1ffb9be79b39726c1
Priya Chauhan <priyachauhan.kkr@gmail.com>
2023-08-29 17:57:03 +0530

    Add BMI Calculator app source code

---
+++ app/src/main/java/com/techmania/bmicalculator/BMIFragment.kt
@@ -0,0 +1,33 @@
+package com.techmania.bmicalculator
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+
+
+class BMIFragment : Fragment() {
+
+    lateinit var result : TextView
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val view:View=inflater.inflate(R.layout.fragment_b_m_i, container, false)
+
+        result = view.findViewById(R.id.textViewResult)
+        val weight=arguments?.getInt("weight")!!.toInt()
+        val height=arguments?.getInt("height")!!.toInt()
+
+        val bmi : Double = ((weight*10000)/(height*height)).toDouble()
+
+        result.text="Your BMI is $bmi"
+        // Inflate the layout for this fragment
+        return view
+    }
+
+
+}
